1afcf373b2668b767d3ab43eb7dd412e8c04628d
o-ifeanyi <onuifeanyi95@gmail.com>
2023-09-13 00:02:22 +0100

    Update auth_repository.dart

--- lib/app/domain/auth_repository.dart
+++ lib/app/domain/auth_repository.dart
@@ -6,13 +6,15 @@ abstract class AuthRepository {
   Future<Result<bool>> logout();
   Future<Result<UserModel>> silentSignIn();
   Future<Result<UserModel>> signIn({
-    required String email,
+    required String emailOrMatNo,
     required String password,
   });
   Future<Result<UserModel>> signUp({
     required String firstName,
     required String lastName,
     required String department,
+    required String faculty,
+    required String matricNo,
     required String email,
     required String password,
   });
